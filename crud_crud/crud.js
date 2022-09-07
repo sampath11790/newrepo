72f9b0da24399294a17830801fd4982778303e44
sampath11790 <bsampath11790@gmail.com>
2022-09-07 20:08:56 +0530

    crud_modif

--- crud_crud/crud.js
+++ crud_crud/crud.js
@@ -27,24 +27,30 @@ const ul=document.getElementById("list-item")
 }
 //get user details from server 
 axios.get('https://crudcrud.com/api/e6a3bf6fa9ad493fb9990a39926181da/myusers')
-.then(res=>print(res.data))
+.then(res=>{
+    
+    //print(res.data)
+    for(var i=0;i<res.data.length;i++){
+        createnode(res.data[i])
+    }
+})
 .catch(err=>console.log(err))
 
 //print and show user details backend to front end
-function print(data){
-    for(var i=0;i<data.length;i++){
-        //console.log(data[i].)
-        createnode(data[i]._id,data[i].username,data[i].email,data[i].phonenumber)
+// function print(data){
+//     for(var i=0;i<data.length;i++){
+//         //console.log(data[i].)
+//         createnode(data[i]._id,data[i].username,data[i].email,data[i].phonenumber)
 
-    }
-}
-function createnode(id,name,email,phone){
+//     }
+// }
+function createnode(obj){
     //console.log(curval.email)
-   // console.log(id)
+  console.log(obj)
 
-    var node=`<li id="${id}">${name}-${email}-${phone}
-    <button onclick=edituser("${id}","${name}","${email}","${phone}")   style="background-color:lightblue;">Edit</button> 
-    <button onclick=deleteuser("${id}")  style="color:red;">Delete User</button>
+    var node=`<li id="${obj._id}">${obj.username}-${obj.email}-${obj.phonenumber}
+    <button onclick=edituser("${obj._id}","${obj.username}","${obj.email}","${obj.phonenumber}")   style="background-color:lightblue;">Edit</button> 
+    <button onclick=deleteuser("${obj._id}")  style="color:red;">Delete User</button>
 
    
     </li>`
@@ -105,61 +111,6 @@ function showscreen(e){
 
     
  }
-// //console.log(localStorage[1])
-// //print
 
-
-// // for(var i=0;i<localStorage.length;i++){
-// //   const beforstring=localStorage.getItem(localStorage.key(i))
-// //  const  normalobj=JSON.parse(beforstring)
-
-// //     console.log(normalobj)
-// // }
-
-// //getback from local storage 
-
-// window.addEventListener("DOMContentLoaded",showscreen)
-// const local=localStorage
-// function showscreen(){
-//     const Storage=Object.keys(localStorage)
-//     console.log(Storage)
-//     for(var i=0;i<Storage.length;i++){
-//         const key=Storage[i]
-//         const curval=local[key]
-//         const finalobj=JSON.parse(curval)
-
-//         //console.log(finalobj)
-//         createfunction(finalobj)
-        
-//     }
-// }
-
-// function createfunction(my_value){
-//     //console.log(curval.email)
-
-//     var node=`<li id="${my_value.email}">${my_value.username}-${my_value.email}-${my_value.phonenumber}
-    
-//     <button onclick=deleteuser("${my_value.email}") style="color:red;">Delete User</button>
-
-//     <button onclick=edituser("${my_value.username}","${my_value.email}","${my_value.phonenumber}") style="background-color:lightblue;">Edit</button>
-//     </li>`
-   
-//    ul.innerHTML=ul.innerHTML+node
-//    //ul.append(node)
-
-// }
-// function deleteuser(a){
-//    localStorage.removeItem(a)
-// }
-// function edituser(a,b,c){
-//     const name=document.getElementById("username")
-//     const mail=document.getElementById("email")
-//     const phone=document.getElementById("phonenumber")
-//     name.value=a
-//     mail.value=b
-//     phone.value=c
-// deleteuser(b)
-
-// }
 
 
