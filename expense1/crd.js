7869296a9a148f34e1c663c4b03380f7847505eb
sampath11790 <bsampath11790@gmail.com>
2022-09-22 18:56:32 +0530

    add screeendelete and async wait after interview

--- expense1/crd.js
+++ expense1/crd.js
@@ -4,38 +4,44 @@ const ul=document.getElementById('list')
 
 form.addEventListener('submit',createobj)
 
-function createobj(e){
-    e.preventDefault()
-    const expense=e.target.expense.value
-    const discription=e.target.discription.value
-    const select=e.target.select.value
-    const obj={
-        expense,
-        discription,
-        select
-    }
-   // console.log(obj)
-axios.post('https://crudcrud.com/api/784925f429184674ab724834100971bb/expence',obj)
-.then(res=>{
-    show(res.data)
-})
-.catch(err=>console.log(err))
+async function createobj(e){
+    try{
+        e.preventDefault()
+        const expense=e.target.expense.value
+        const discription=e.target.discription.value
+        const select=e.target.select.value
+        const obj={
+            expense,
+            discription,
+            select
+        }
+       // console.log(obj)
+    const postobj=await axios.post('https://crudcrud.com/api/946392c86541478d8ad6a40fe8f08164/expence',obj)
+      show(postobj.data)
+    } 
+catch(err){
+    console.log(err)
+}
 }
 
-
 //get from server
-axios.get('https://crudcrud.com/api/784925f429184674ab724834100971bb/expence')
-.then((res)=>{
-    const arr=res.data
-    for(var i=0;i<arr.length;i++){
-       printscreen(arr[i])    
+async function get(){
+    try{
+        const getele= await axios.get('https://crudcrud.com/api/946392c86541478d8ad6a40fe8f08164/expence')
+        const arr=getele.data
+        for(var i=0;i<arr.length;i++){
+           printscreen(arr[i]) 
     }
-})
-.catch(err=>console.log(err))
+   
+}catch(err){
+  console.log(err)
+}
+}
+get()
 
 //print screen
-function show(e){
-  
+async function show(e){
+  try{
     const expense=e.expense
     const discription=e.discription
     const select=e.select
@@ -61,36 +67,69 @@ li.appendChild(editbtn)
   ul.appendChild(li)
 
   //ul.innerHTML=ul.innerHTML+li
+  }
+  catch(err){
+    console.log(err)
+  }
+    
 
 }
 
 
 //server to screen
-function printscreen(arr){
-    const parrentnode=document.getElementById('list')
-    const node=`<li>${arr.expense}-${arr.discription}-${arr.select}
-    <button onclick=edit("${arr._id}","${arr.expense}","${arr.discription}","${arr.select}")>edit</button>
-    <button onclick=delet("${arr._id}")>delete</button>
-    </li>`
-    parrentnode.innerHTML=parrentnode.innerHTML+node
+async function printscreen(arr){
+    try{
+        const parrentnode=document.getElementById('list')
+        const node=`<li id="${arr._id}">${arr.expense}-${arr.discription}-${arr.select}
+        <button onclick=edit("${arr._id}","${arr.expense}","${arr.discription}","${arr.select}")>edit</button>
+        <button onclick=delet("${arr._id}")>delete</button>
+        </li>`
+        parrentnode.innerHTML=parrentnode.innerHTML+node
+    }
+    catch(err){
+     console.log(err)
+    }
+    
 }
 
 
 //delete btn function
 
-function delet(id){
-   axios.delete(`https://crudcrud.com/api/784925f429184674ab724834100971bb/expence/${id}`)
-   .then(res=>console.log("this user removed from the cloud"))
-   .catch(err=>console.log("err"))
+async function delet(id){
+    try{
+       const objdel= await axios.delete(`https://crudcrud.com/api/946392c86541478d8ad6a40fe8f08164/expence/${id}`)
+       screendelete(id)
+    }
+    
+   catch(err){
+     console.log(err)
+   }
+}
+
+async function screendelete(id){
+    try{
+        const li=document.getElementById(id)
+        console.log(li)
+        li.remove()
+    }catch(err){
+        console.log(err)
+    }
+   
+    //console.log("hi screen delete")
 }
 
 //edit function
-function edit(uid,uexpense,udiscription,uselect){
-    const expense=document.getElementById("expense")
-    const discription=document.getElementById("discription")
-    const select=document.getElementById("select")
-    expense.value=uexpense
-    discription.value=udiscription
-    select.value=uselect
-    delet(uid)
+async function edit(uid,uexpense,udiscription,uselect){
+    try{
+        const expense=document.getElementById("expense")
+        const discription=document.getElementById("discription")
+        const select=document.getElementById("select")
+        expense.value=uexpense
+        discription.value=udiscription
+        select.value=uselect
+        delet(uid)
+    }catch(error){
+       console.log(error)
+    }
+    
 }
